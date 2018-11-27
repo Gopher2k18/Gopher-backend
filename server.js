aa9110b32a8197e719e6b05fb7e1b8ebd22c28d9
pessi22 <juhopessi1@gmail.com>
2018-11-27 10:55:34 +0200

    port changed

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ app.get('/', (req, res) => {
 require('./app/routes/routes.js')(app);
 
 // listen for requests
-app.listen(process.env.PORT || 4000, () => {
-    console.log("Server is listening on port 4000");
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is listening on port 3000");
 });
